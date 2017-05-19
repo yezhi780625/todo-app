585d16d5aed5eac9ba9ebf4eed8885b7fc891d70
yezhi <yezhi@cht.com.tw>
2017-05-19 14:44:15 +0800

    rebuild

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Root from './containers/Root';
-import './styles/index.css';
-
-ReactDOM.render(
-  <Root />,
-  document.getElementById('root')
-);
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import todo from './ToDoReducer';
-import app from './AppReducer';
-
-export default combineReducers({
-  app,
-  todo
-});
